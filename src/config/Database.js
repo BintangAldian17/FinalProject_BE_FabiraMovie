1809763093c824c29f8db887bdeacbea89d81a6a
BintangAldian17 <bintangaldian17@gmail.com>
2023-05-24 23:44:15 +0700

    setUp deploy(2)

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv")
+dotenv.config({ path: "./.env" })
 
-const db = new Sequelize('fabira_movie', 'root', '@murdi254313', {
-    host: 'localhost',
-    dialect: 'mysql'
+const db = new Sequelize(process.env.NAME_DB, process.env.USER_DB, process.env.PASSWORD_DB, {
+    host: process.env.HOST_DB,
+    dialect: 'mysql',
+    port: process.env.PORT_DB
 });
 
 module.exports = db;
