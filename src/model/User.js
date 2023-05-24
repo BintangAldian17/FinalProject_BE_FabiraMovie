0b607da67599d1c1003271275671b72ecd8acd3b
BintangAldian17 <bintangaldian17@gmail.com>
2023-05-24 22:57:50 +0700

    Add existing project myFinalProject HSB to deployment

---
+++ src/model/User.js
@@ -0,0 +1,34 @@
+const db = require('../config/Database.js')
+const { Sequelize } = require('sequelize')
+
+const { DataTypes } = Sequelize
+
+const Users = db.define('users', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    avatar: {
+        type: DataTypes.STRING,
+        defaultValue: 'user.png',
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    freezeTableName: true
+})
+
+
+module.exports = Users
