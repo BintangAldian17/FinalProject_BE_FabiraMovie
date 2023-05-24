c15f0b8231b02aff805eb410a7194f9e826b91a4
BintangAldian17 <bintangaldian17@gmail.com>
2023-05-25 00:05:11 +0700

    setUp cookie

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ const cors = require('cors');
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
 const db = require('../src/config/Database')
-
-
 const router = require('./routes/router');
-const Favorite = require('./model/Favorite');
+
 //untuk menggunakan .env
 dotenv.config()
 const app = express();
 
+// ketika di deploy port berdasarkan hostingan
+const port = process.env.PORT || 8800
 
 try {
+    db.sync()
     console.log("Database Connected");
 } catch (error) {
     console.error("Unable to connect to the database:", error);
@@ -28,4 +29,4 @@ app.use(cors({ origin: ['http://localhost:3000', 'https://fabiramovie.vercel.app
 app.use('/', router);
 
 
-app.listen(8800, () => { console.log('Server Running') });
+app.listen(port, () => { console.log(`Server Running ${port}`) });
